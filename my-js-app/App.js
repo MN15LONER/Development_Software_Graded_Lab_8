28cef9747c52f55beb4cfa59233e3588731c3e47
MN15LONER <mfunekondeya83@gmail.com>
2025-10-01 13:35:53 +0200

    edits

--- my-js-app/App.js
+++ my-js-app/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { View, StyleSheet } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
+import { ProjectProvider } from './context/ProjectContext';
 
 export default function App() {
   return (
-    <>
-      <StatusBar style="auto" />
-      <AppNavigator />
-    </>
+    <ProjectProvider>
+      <View style={styles.container}>
+        <AppNavigator />
+        <StatusBar style="auto" />
+      </View>
+    </ProjectProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
